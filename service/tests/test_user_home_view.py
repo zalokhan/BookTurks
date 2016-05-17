60de5dd897bafd617c1c8cd16cdc4b0a59b5ec6b
Zeeshan Lokhandwala <zlokhandwala@zlokhandwa-m01.vmware.com>
2016-05-17 15:07:48 -0700

    Added tests for user home view

--- service/tests/test_user_home_view.py
+++ service/tests/test_user_home_view.py
@@ -1,5 +1,8 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth.models import User
 
 
 class UserHomeViewTest(TestCase):
@@ -13,6 +16,19 @@ class UserHomeViewTest(TestCase):
         :return:
         """
         client = Client()
-        response = client.get(reverse('service:user_home'))
-        # TODO: Fix this.
-        self.assertEqual(response.status_code, 302)
+        context = {
+            'username': 'test@email.com',
+            'user_first_name': 'testfirstname',
+            'user_last_name': 'testlastname',
+            'user_phone': '1234567890',
+            'user_dob': '01/01/1990',
+            'password': 'test',
+            'repassword': 'test'
+        }
+        User.objects.create_user(username=context.get('username'), email=context.get('username'),
+                                 password=context.get('password'))
+        user = authenticate(username=context.get('username'), password=context.get('password'))
+        self.assertEqual(user.is_active, True)
+        client.login(username=context.get('username'), password=context.get('password'))
+        response = client.post(reverse('service:user_home'), context)
+        self.assertEqual(response.status_code, 200)
