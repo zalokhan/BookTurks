01a125f793ec2f79eebce78950b24237899ac451
Zeeshan Lokhandwala <zlokhandwala@zlokhandwa-m01.vmware.com>
2016-05-18 00:16:41 -0700

    Added quiz tests; create method remaining

--- service/tests/test_user_quiz_view.py
+++ service/tests/test_user_quiz_view.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 from service.tests.create_user import create_user, context
+from service.models import Quiz
 
 
 class UserQuizViewTest(TestCase):
@@ -35,8 +36,62 @@ class UserQuizViewTest(TestCase):
 
         self.assertEqual(user.is_active, True)
         client.login(username=context.get('username'), password=context.get('password'))
-        response = client.post(reverse('service:user_quiz_maker'), quiz_parameters)
+        response = client.post(reverse('service:user_quiz_maker'), quiz_parameters, follow=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_user_quiz_maker_view_with_empty_id(self):
+        """
+        Testing the user maker quiz view
+        :return:
+        """
+        client = Client()
+        user = create_user()
+        quiz_parameters = dict(context)
+
+        quiz_parameters['quiz_id'] = ''
+        quiz_parameters['quiz_name'] = 'test_quiz_name'
+        quiz_parameters['quiz_description'] = 'test_quiz_description'
+
+        self.assertEqual(user.is_active, True)
+        client.login(username=context.get('username'), password=context.get('password'))
+        response = client.post(reverse('service:user_quiz_maker'), quiz_parameters, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        # Testing redirection
+        redirect_chain = list()
+        redirect_chain.append(("/quiz/init/", 302))
+        self.assertEqual(response.redirect_chain, redirect_chain)
+
+    def test_user_quiz_maker_view_with_duplicate_id(self):
+        """
+        Testing the user maker quiz view
+        :return:
+        """
+        client = Client()
+        user = create_user()
+        quiz_parameters = dict(context)
+
+        quiz_parameters['quiz_id'] = 'test_quiz_id'
+        quiz_parameters['quiz_name'] = 'test_quiz_name'
+        quiz_parameters['quiz_description'] = 'test_quiz_description'
+        quiz = Quiz(
+            quiz_id='test_quiz_id',
+            quiz_name='test',
+            quiz_description='test description',
+            quiz_owner='test@email.com')
+        quiz.save()
+
+        self.assertEqual(user.is_active, True)
+        client.login(username=context.get('username'), password=context.get('password'))
+        response = client.post(reverse('service:user_quiz_maker'), quiz_parameters, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        # Testing redirection
+        redirect_chain = list()
+        redirect_chain.append(("/quiz/init/", 302))
+        self.assertEqual(response.redirect_chain, redirect_chain)
+        # deleting to provide clean slate to other tests
+        quiz.delete()
 
     def test_user_quiz_verifier_view_with_null_inputs(self):
         """
