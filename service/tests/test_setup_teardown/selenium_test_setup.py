e8f21c32745365e9dec5656ae87b8e141e6115c1
Zeeshan Lokhandwala <zalokhan@ncsu.edu>
2016-09-16 10:25:06 -0700

    Changing selenium env

--- service/tests/test_setup_teardown/selenium_test_setup.py
+++ service/tests/test_setup_teardown/selenium_test_setup.py
@@ -1,8 +1,8 @@
 import os
 
-import selenium.webdriver
 from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
 from selenium.webdriver.firefox.webdriver import WebDriver
 
 
@@ -20,8 +20,8 @@ def get_sauce_driver(capabilities):
     capabilities['tags'] = [os.environ['TRAVIS_PYTHON_VERSION'], 'CI']
     capabilities['browserName'] = 'firefox'
 
-    return selenium.webdriver.Remote(desired_capabilities=capabilities,
-                                     command_executor="http://{0}/wd/hub".format(hub_url))
+    return webdriver.Remote(desired_capabilities=capabilities,
+                            command_executor="http://{0}/wd/hub".format(hub_url))
 
 
 def get_local_driver():
@@ -29,7 +29,9 @@ def get_local_driver():
     Returns local web driver
     :return:
     """
-    return WebDriver()
+    if 'TRAVIS' in os.environ:
+        return WebDriver()
+    return webdriver.Chrome(settings.BASE_DIR + '/service/tests/chromedrivers/chromedriver_mac64')
 
 
 class SeleniumTests(StaticLiveServerTestCase):
@@ -37,6 +39,7 @@ class SeleniumTests(StaticLiveServerTestCase):
     Base setup and tear down for integration tests.
     Distribute into different files if integration tests grow.
     """
+
     @classmethod
     def setUpClass(cls):
         super(SeleniumTests, cls).setUpClass()
